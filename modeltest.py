9d4b417978b6eae9c5f2a71f21044e91fcec99fb
sizeof <aviv@mac17.local>
2009-06-04 18:34:35 -0700

    a

---
+++ modeltest.py
@@ -0,0 +1,69 @@
+import datetime, uuid, hashlib, random, string
+
+gen_uuid    = lambda: uuid.uuid4().bytes
+pretty_hex  = lambda b: str(uuid.UUID(bytes=b))
+sha1        = lambda s: hashlib.sha1(s).digest()
+
+class User(object):
+    def __init__(self, **kw):
+        if kw:
+            self._init_new(kw)
+    
+    def _init_new(self, kw):
+        for f in ('name', 'username', 'password', 'email'):
+            if f in kw:
+                self.__setattr__(f, kw.pop(f))
+            else:
+                self.__setattr__(f, u'')
+        
+        self.created = datetime.datetime.utcnow()
+        self.options = {
+            'new_window': True,
+            'lang': 'en',
+            'private': kw.get('private', False)
+        }
+        self.services = {}
+        self.notifications = []
+
+        self.id = gen_uuid()
+        
+        self.new = self.modified = True
+        
+    @property
+    def should_save(self):
+        """True if the user should be saved to db."""
+        return self.modified or self.new
+    
+    def password():
+        doc = """Sets user password and auto-crypts it"""
+        def fget(self):
+            return self._password
+        def fset(self, v):
+            self._password = sha1(v)
+            self.modified = True
+        return locals()
+    password = property(**password())
+    
+    def check_password(self, against):
+        """Match a user-provided password against the current, encrypted, one"""
+        return self._password == sha1(against)
+    
+    def uuid():
+        doc = """Get or set user's UUID"""
+        def fget(self):
+            return pretty_hex(self.id)
+        return locals()
+    uuid = property(**uuid())
+    
+    def as_new_mongo_dict(self):
+        fields = ('name', 'username', 'password', 'email', 'options', 'services', 'notifications', 'created')
+        values = map(self.__getattribute__, fields)
+        
+        dic = dict(zip(fields, values))
+        
+        dic['_id'] = self.id
+        
+        return dic
+    
+    def __repr__(self):
+        return "<User('%s', '%s')>" % (self.username, self.name)
